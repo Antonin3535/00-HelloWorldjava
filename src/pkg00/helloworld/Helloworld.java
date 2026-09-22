c83d0a5bcf7a5c51ca530a6d7ff1324853fc713c
Antonin <168985784+Antonin3535@users.noreply.github.com>
2026-09-22 11:03:33 +0200

    Update Helloworld.java

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -14,7 +14,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World by AS");
+        System.out.println("Hello World by AS on Github");
     }
     
 }
