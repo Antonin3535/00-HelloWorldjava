a95d7201e09ce22c58728636fc2d0e0ed03d774d
asautier <asautier@350028R-F115X17.lise.kreizenn.bzh>
2026-09-22 12:35:56 +0200

    modification message printf

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -14,7 +14,11 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World by AS on Github");
+        //Ex01
+        System.out.println("Bonjour\nJava\nCode");
+        /*
+       
+        
     }
     
 }
